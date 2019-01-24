38dde3fda3cf69998dcfca10198d781312578b1f
diymine <diymine@163.com>
2019-01-24 23:36:39 +0800

    int

---
+++ install packages.bat
@@ -0,0 +1,1 @@
+npm install --save-dev typescript webpack ts-loader css-loader vue vue-loader vue-template-compiler webpack-cli
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// src/index.ts
+
+import Vue from "../node_modules/vue";
+
+let v = new Vue({
+    el: "#app",
+    template: `
+    <div>
+        <div>Hello {{name}}!</div>
+        Name: <input v-model="name" type="text">
+    </div>`,
+    data: {
+        name: "World"
+    }
+});
